e13ac80ee02dd2a698a689b0a18607495cbce6b7
ethankim612 <ethansw612@gmail.com>
2025-02-20 23:01:22 +0900

    Ethan-11:01

--- csiatech/csiatech/settings.py
+++ csiatech/csiatech/settings.py
@@ -193,7 +193,7 @@ STATICFILES_DIRS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/collectedstatic/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
