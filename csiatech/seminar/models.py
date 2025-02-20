973b637f0bc72a6a478e8f020891f3887f128b52
ethankim612 <ethansw612@gmail.com>
2025-02-20 15:56:47 +0900

    Ethan-3:56

--- csiatech/seminar/models.py
+++ csiatech/seminar/models.py
@@ -1,3 +1,23 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    student1 = models.CharField(max_length=20, blank=True)
+    student2 = models.CharField(max_length=20, blank=True)
+    student3 = models.CharField(max_length=20, blank=True)
+    student4 = models.CharField(max_length=20, blank=True)
+    student5 = models.CharField(max_length=20, blank=True)
+    student6 = models.CharField(max_length=20, blank=True)
+    period1 = models.BooleanField(default=False)
+    period2 = models.BooleanField(default=False)
+    period3 = models.BooleanField(default=False)
+    room_number = models.IntegerField()
+
+
+class Room(models.Model):
+    room_number = models.IntegerField()
+    period1 = models.BooleanField(default=False)
+    period2 = models.BooleanField(default=False)
+    period3 = models.BooleanField(default=False)
+  
