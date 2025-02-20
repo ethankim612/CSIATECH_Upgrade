6598feec0234f1113c72e7e9dd259a842d7639ba
ethankim612 <ethansw612@gmail.com>
2025-02-20 22:18:32 +0900

    Ethan-10:18

--- csiatech/club/views.py
+++ csiatech/club/views.py
@@ -7,4 +7,6 @@ def club_view(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect("https://csiatech.kr/")
 
-    return render(request, "club.html" ,{"student_id": request.user.student_id})
+    context = {}
+    context["student_id"] = request.user.student_id
+    return render(request, "club.html")
