7c52235d4f71da0a24b57334359529368ca47787
ethankim612 <ethansw612@gmail.com>
2025-02-25 17:58:28 +0900

    home_sdjfsdf

--- csiatech/csiatech/settings.py
+++ csiatech/csiatech/settings.py
@@ -65,7 +65,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_BEAT_SCHEDULE = {
     "run-reset-every-friday": {
         "task": "yaja.tasks.run_reset_script",
-        "schedule": crontab(hour=13, minute=59, day_of_week="sat"),
+        "schedule": crontab(hour=, minute=59, day_of_week="sat"),
     },
     "run-reset-every-day-9am": {
         "task": "yaja.tasks.run_update_script",
