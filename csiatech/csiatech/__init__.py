53a2dad96772f582615e40ee398541b086cfd8e4
ethankim612 <ethansw612@gmail.com>
2025-03-03 13:50:19 +0900

    commit_init_ upda3

--- csiatech/csiatech/__init__.py
+++ csiatech/csiatech/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
 # Import Celery app
-from .celery_app import app as celery_app
+from csiatech.celery_app import app as celery_app
 
 __all__ = ('celery_app',)
