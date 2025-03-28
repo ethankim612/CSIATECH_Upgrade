ed2a53b5b0014472db6d85ecbbb9aac9f83564df
ethankim612 <ethansw612@gmail.com>
2025-03-28 11:39:55 +0900

    update_studentid_rec

--- csiatech/yaja/models.py
+++ csiatech/yaja/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from simple_history.models import HistoricalRecords
@@ -8,7 +7,7 @@ class Monday(models.Model):
     period1 = models.CharField(max_length=50)
     period2 = models.CharField(max_length=50)
     period3 = models.CharField(max_length=50)
-    student_id = models.CharField(max_length=10, null=True, blank=True)
+    student_id = models.CharField(max_length=10, null=True, blank=True, unique=True)
     history = HistoricalRecords()
 
 
@@ -16,7 +15,7 @@ class Tuesday(models.Model):
     period1 = models.CharField(max_length=50)
     period2 = models.CharField(max_length=50)
     period3 = models.CharField(max_length=50)
-    student_id = models.CharField(max_length=10, null=True, blank=True)
+    student_id = models.CharField(max_length=10, null=True, blank=True, unique=True)
     history = HistoricalRecords()
 
 
@@ -24,7 +23,7 @@ class Wednesday(models.Model):
     period1 = models.CharField(max_length=50)
     period2 = models.CharField(max_length=50)
     period3 = models.CharField(max_length=50)
-    student_id = models.CharField(max_length=10, null=True, blank=True)
+    student_id = models.CharField(max_length=10, null=True, blank=True, unique=True)
     history = HistoricalRecords()
 
 
@@ -32,6 +31,6 @@ class Thursday(models.Model):
     period1 = models.CharField(max_length=50)
     period2 = models.CharField(max_length=50)
     period3 = models.CharField(max_length=50)
-    student_id = models.CharField(max_length=10, null=True, blank=True)
+    student_id = models.CharField(max_length=10, null=True, blank=True, unique=True)
     history = HistoricalRecords()
 
